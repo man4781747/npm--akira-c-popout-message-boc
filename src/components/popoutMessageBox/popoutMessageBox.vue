11b301aaa662c0bfed6360a93c7041b8329238d6
Akira Chen <man4781747@gmail.com>
2025-06-18 15:28:19 +0800

    css檔案獨立

--- src/components/popoutMessageBox/popoutMessageBox.vue
+++ src/components/popoutMessageBox/popoutMessageBox.vue
@@ -26,6 +26,7 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { popoutMessage } from './popoutMessageService'
+import './popoutMessageBox.css'
 
 export default defineComponent({
   name: 'PopoutMessageBox',
@@ -127,117 +128,4 @@ export default defineComponent({
 </script>
 
 <style lang="postcss">
-#popout-message-box-list {
-  position: fixed;
-  top: 3rem;
-  right: 0;
-  z-index: 99999;
-  margin: 0.5rem;
-  max-width: 400px;
-  max-height: calc(100vh - 3rem - 8px);
-  overflow-x: hidden;
-  overflow-y: auto;
-  &::-webkit-scrollbar {
-    width: 4px;
-  }
-  & .popout-message-box {
-    min-width: 10rem;
-    max-width: 25vw;
-    cursor: pointer;
-    opacity: 0.3;
-    transition: all 0.7s;
-    &:first-child {
-      opacity: 1;
-    }
-    &:hover {
-      opacity: 1;
-    }
-    & .card {
-      max-height: 100vh;
-      box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.1);
-      border: 0;
-      border-style: solid;
-      border-width: 0px;
-      border-left-width: 5px;
-      transition: all 0.5s;
-      &.success {
-        border-color: var(--bs-green);
-      }
-      &.fail {
-        border-color: var(--bs-red);
-      }
-      &.info {
-        border-color: #289dc9;
-      }
-      &.warning {
-        border-color: var(--bs-warning);
-      }
-      &.primary {
-        border-color: var(--bs-primary);
-      }
-      &.debug {
-        border-color: var(--bs-secondary);
-      }
-      margin-bottom: 8px;
-
-      & .card-body {
-        & .card-title {
-          font-size: 1rem;
-          font-weight: bold;
-          letter-spacing: 0.0125em;
-          border: 0;
-          margin: 0;
-        }
-        & .card-desp {
-          font-size: 0.8rem;
-          letter-spacing: 0.0125em;
-          border: 0;
-          color: var(--bs-gray-500);
-        }
-      }
-    }
-  }
-
-  .when-create-leave-active,
-  .when-create-enter-active {
-    transition: all 0.5s ease;
-    transform: translateX(0%) translateY(0px);
-    margin-bottom: 8px;
-  }
-
-  .when-create-enter-from {
-    background-color: rgba(0, 0, 0, 0);
-    transform: translateX(120%) translateY(0px);
-    height: 0px;
-    margin-bottom: 0px;
-  }
-
-  .when-create-leave-to {
-    background-color: rgba(0, 0, 0, 0);
-    transform: translateX(114%) translateY(-4px);
-    height: 0px;
-    margin-bottom: 0px;
-  }
-
-  .time-mask {
-    position: absolute;
-    left: 0;
-    top: 0;
-    bottom: 0;
-    right: 100%;
-    background-color: rgba(0, 0, 0, 0.1);
-    &.stop {
-      animation-play-state: paused !important;
-    }
-  }
-}
-
-@keyframes time_mask_anime {
-  from {
-    right: 100%;
-  }
-  to {
-    right: 0%;
-  }
-}
 </style>
